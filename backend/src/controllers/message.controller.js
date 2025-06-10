1b219f084a19880afddd5b3f185741fede00a5a6
Aleexj <jalias59@outlook.fr>
2025-06-10 16:14:23 +0200

    Ajoute la fonctionnalité de récupération des messages entre utilisateurs

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -8,8 +9,24 @@ export const getUsersForSidebar = async (req, res) => {
     );
 
     res.status(200).json(filteredUsers);
-    
   } catch (error) {
     console.log("Error in getUsersForSidebar controller", error);
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: receiverUserId } = req.params;
+    const myId = req.user._id;
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: receiverUserId },
+        { senderId: receiverUserId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller", error);
   }
 };
