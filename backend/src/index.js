bca5ec369aa7b9cbd35124943f637f04e3fe7e50
Aleexj <jalias59@outlook.fr>
2025-06-10 13:39:38 +0200

    Ajoute la configuration de dotenv pour gérer les variables d'environnement et modifie le port d'écoute du serveur Express pour utiliser la variable d'environnement PORT.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 
 import authRoute from "./routes/auth.route.js";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use("/api/auth", authRoute);
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
