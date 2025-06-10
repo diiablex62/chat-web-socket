1f4b6892a72f97a38209143e75c16bf094af7dd7
Aleexj <jalias59@outlook.fr>
2025-06-10 16:05:26 +0200

    Ajoute la fonctionnalité de récupération des utilisateurs pour la barre latérale avec une nouvelle route

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoute from "./routes/auth.route.js";
+import messageRoute from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
+app.use("/api/message", messageRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
