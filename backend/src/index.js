ac94762886ada894f8258d321f53b0e836f5e28a
Aleexj <jalias59@outlook.fr>
2025-06-10 13:56:56 +0200

    Met à jour les dépendances du projet et ajuste les fichiers de configuration pour améliorer la compatibilité et la performance de l'application.

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoute from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -12,4 +13,5 @@ app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
