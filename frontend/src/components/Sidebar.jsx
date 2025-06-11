e972fcc030d26649369aa55b454495c018f559a0
Aleexj <jalias59@outlook.fr>
2025-06-11 16:00:12 +0200

    Update profile image source in Sidebar component to use profileAvatar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -42,7 +42,7 @@ export default function Sidebar() {
           >
             <div className="relative mx-auto lg:mx-0">
               <img
-                src={user.profilePic || "/avatar.png"}
+                src={user.profileAvatar || "/avatar.png"}
                 alt={user.name}
                 className="size-12 object-cover rounded-full"
               />
