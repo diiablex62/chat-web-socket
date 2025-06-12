894eae41493cda7202639aeefda832ad8d8eade4
Aleexj <jalias59@outlook.fr>
2025-06-12 09:10:46 +0200

    Add image upload and message sending functionality to MessageInput component; enhance Zustand store with sendMessage method

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,13 +2,12 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
   isUsersLoading: false,
   isMessagesLoading: false,
-  
 
   getUsers: async () => {
     set({ isUsersLoading: true });
@@ -31,6 +30,21 @@ export const useChatStore = create((set) => ({
       toast.error(error.response.data.message);
     } finally {
       set({ isMessagesLoading: false });
+    }
+  },
+
+  sendMessage: async (messagedata) => {
+    const { selectedUser } = get();
+    try {
+      const res = await axiosInstance.post(
+        `/message/send/${selectedUser._id}`,
+        messagedata
+      );
+
+      set({ messages: [...messages, res.data] });
+      toast.success("Message sent successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
     }
   },
 
